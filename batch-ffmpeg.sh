24d20080ade89a439ccaf5c3de076f3c1cdf1b90
Jonathan Lango <105533393+jlango0@users.noreply.github.com>
2025-02-17 00:53:07 -0500

    feat: Calculate number of .wav files in directory
    Remove hard-coded value of .wav files [i.e. {1..28}] from initial commit.
    Add [for] loop, C language-type

--- batch-ffmpeg.sh
+++ batch-ffmpeg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# for initial commit, leaving the track numbers hard-coded
-# will fix going forward
-for i in {1..28}; do
+# Determine how many .wav files are in the current directory
+qty_of_wav_files=$(ls -l *.wav | wc -l)
+
+# Potential error if first file is "Track 01.wav" instead of "Track 1.wav"
+for (( i=1; i<=$qty_of_wav_files; i++ )); do
     ffmpeg -i "Track $i.wav" "Track $i.mp3"
 done
